eff831d57c88f9ebd53e5c9f18ea1746d1b0d8ee
Laco test <lo25654@yandex.ru>
2023-05-14 15:12:51 +0300

    First commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String surname;
+    public boolean subscription;
+    public String phone;
+    public FormDate birthday;
+}
